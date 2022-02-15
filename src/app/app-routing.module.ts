830b0e7985aa4d899982d1d78f248aacd761e8fa
Aliciaegea <aliegea4@gmail.com>
2022-02-15 11:39:10 +0100

    removing unused code and folders and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
+import { QRCodeComponent } from "angular2-qrcode";
 
-import { ContactComponent } from "./contact/contact.component";
 import { QrGeneratorComponent } from "./qr-generator/qr-generator.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: HomeComponent,
+    component: QrGeneratorComponent,
     pathMatch: "full",
     data: {
       title: "Generador Qr gratis Inicio",
-      description: "Genera to código QR gratos online",
-      ogTitle: "generador de código Qr online y gratuito",
-    },
-  },
-  {
-    path: "home",
-    component: HomeComponent,
-    data: {
-      title: "Generador Qr gratis home",
       description: "Genera to código QR gratos online",
       ogTitle: "generador de código Qr online y gratuito",
     },
@@ -34,15 +24,6 @@ const routes: Routes = [
       description:
         "personaliza tu código QR, escoge color , tamaño y contenido",
       ogTitle: "diseña tu código Qr online y gratuito",
-    },
-  },
-
-  {
-    path: "contact",
-    component: ContactComponent,
-    data: {
-      title: "contacto",
-      description: "formulario de contacto y dirección generador QR ",
     },
   },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,19 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
-import { HomeComponent } from "./home/home.component";
-
-import { ContactComponent } from "./contact/contact.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { QRCodeModule } from "angular2-qrcode";
 import { ColorPickerModule } from "ngx-color-picker";
 import { DatospersonalesComponent } from "./datospersonales/datospersonales.component";
 import { QrGeneratorComponent } from "./qr-generator/qr-generator.component";
-
 import { UrlComponent } from "./qr-generator/url/url.component";
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
-
-    ContactComponent,
     DatospersonalesComponent,
     QrGeneratorComponent,
-
     UrlComponent,
   ],
   imports: [
